3337dd897811bc9484f32b85aa6c0de1afea3c29
janhvigupta01 <janhvigupta5555@gmail.com>
2026-05-06 21:40:05 +0530

    Updated frontend and backend with Docker setup

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import {Company} from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 export const registerCompany=async(req,res)=>{
     try {
         const {companyName}=req.body;
@@ -71,9 +73,14 @@ export const updateCompany=async (req,res)=>{
     try {
         const {name, description, website, location} = req.body;
         const file=req.file;
-        //cloudinary ayega idhr
-
         const updateData={name, description, website, location};
+
+        if (file) {
+            const fileUri = getDataUri(file);
+            const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+            updateData.logo = cloudResponse.secure_url;
+        }
+
         const company=await Company.findByIdAndUpdate(req.params.id,updateData,{new:true});
         if(!company){
             return res.status(404).json({
@@ -83,6 +90,7 @@ export const updateCompany=async (req,res)=>{
         }
         return res.status(200).json({
             message:"Company Information updated",
+            company,
             success:true,
         })  
     } catch (error) {
