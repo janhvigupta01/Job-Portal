74bf7c6fb15477942050fe801f865d2f351fe81c
janhvigupta01 <janhvigupta5555@gmail.com>
2026-05-15 22:09:57 +0530

    fix cors for vercel

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,12 @@ app.get("/home",(req,res)=>{
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions={
-    origin:'http://localhost:5173',
-    credentials:true
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'https://job-portal-snowy-omega.vercel.app'
+    ],
+    credentials: true
 }
 app.use(cors(corsOptions));
 
